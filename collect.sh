d4071ea0f8e5617e8d828eb426a62b778caad944
Uzi Erdenebileg <uerdenebileg@EliteBook-840-G6>
2021-11-05 01:59:23 +0800

    Update

--- collect.sh
+++ collect.sh
@@ -2,17 +2,18 @@
 #!/bin/bash
 
 srcs="
-  ~/.config
-  ~/.xinitrc
-  ~/.fonts
-  ~/.xresources
-  ~/.zshrc
+  "$HOME"/.config/awesome
+  "$HOME"/.config/gtk-3.0
+  "$HOME"/.xinitrc
+  "$HOME"/.fonts
+  "$HOME"/.Xresources
+  "$HOME"/.zshrc
+  /etc/fonts
   /etc/portage
   /usr/src/linux/.config
 "
 [[ ! -z "$1" ]] && dest="$1" || dest="."
-echo "Copying to $dest."
 
-for src in $srcs; do
-  cp -rav ~/.config $dest
-done
+#for src in $srcs; do
+  rsync -rav $srcs $dest
+#done
